1076d582c154b061134b6157b22bca809eab0cd2
Ariana Winters <arianaw15@gmail.com>
2021-01-24 18:53:54 -0700

    Created search button that captures input

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,35 @@
-var cityName = $("#cityName").val().trim();
-var queryURl = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=ffb7d89855d02b19a877cab03ce8e95d"
+var cities= [""]
+
+function renderButtons(){
 $(".searchBtn").on("click", function () {
+    var cityName = $("#cityName").val().trim();
     alert(cityName);
+  console.log(cityName);
+$("#search").append("<button>" + cityName);
+})
+};
+renderButtons();
+$("button").on("click",function(){
+    console.log(this);
 })
 
-function cityBtn() {
+var queryURl =
+  "api.openweathermap.org/data/2.5/forecast?q="+ cities +"&appid=ffb7d89855d02b19a877cab03ce8e95d";
 
-}
+
+// function renderButtons() {
+        
+//     for (let index = 0; index < cities.length; index++) {
+//       const element = cities[index];
+//       $("#search").append("<button>" + element);
+//       console.log(element);
+//     }
+    
+//     $(".searchBtn").on("click", function (event) {
+//         var cityName = $("#cityName").val().trim();
+//         $("#search").push("<button>" + cityName); 
+//       });
+
+//         renderButtons();
+//       };
+//   renderButtons();
