cb164a73ee05061c3cc15d79aee4a0b898bb2b52
Ariana Winters <arianaw15@gmail.com>
2021-01-24 22:20:58 -0700

    updated with API and additional text

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,54 @@
-var cities= [""]
 
+var cities= [""]
 function renderButtons(){
 $(".searchBtn").on("click", function () {
     var cityName = $("#cityName").val().trim();
-    alert(cityName);
+    var newBtn= document.createElement("button");
+    newBtn.textContent= cityName;
   console.log(cityName);
-$("#search").append("<button>" + cityName);
+$("#search").append(newBtn).addClass(".newButton");
+weatherToday();
+weatherForecast();
 })
 };
 renderButtons();
-$("button").on("click",function(){
-    console.log(this);
-})
 
-var queryURl =
-  "api.openweathermap.org/data/2.5/forecast?q="+ cities +"&appid=ffb7d89855d02b19a877cab03ce8e95d";
+var d = new Date();
+function weatherToday(){
+var cityName = $("#cityName").val();
+      var APIKey = "14f18f856625e3dcfa23c9ccca5d5d46";
+      var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+APIKey;
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        $("#name").text(response.name);
+        $(".date").text("Date: " + d);
+        $(".temp").text("Temperature (Kelvin): "+response.main.temp);
+        $(".humidity").text("Humidity: "+ response.main.humidity);
+        $(".weatherEx").text("Current weather conditions: "+response.weather[0].main);
+        // $(".temp").text(response.list.main.temp);
+      })
+    }
+    ;
 
-
-// function renderButtons() {
-        
-//     for (let index = 0; index < cities.length; index++) {
-//       const element = cities[index];
-//       $("#search").append("<button>" + element);
-//       console.log(element);
-//     }
-    
-//     $(".searchBtn").on("click", function (event) {
-//         var cityName = $("#cityName").val().trim();
-//         $("#search").push("<button>" + cityName); 
-//       });
-
-//         renderButtons();
-//       };
-//   renderButtons();
+    function weatherForecast(){
+        var cityName = $("#cityName").val();
+        var APIKey = "14f18f856625e3dcfa23c9ccca5d5d46";
+        var queryURL2= "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&cnt=5&appid="+APIKey;
+        $.ajax({
+            url: queryURL2,
+            method: "GET",
+          }).then(function (response) {
+              console.log(response);
+              var nextDay= d.setDate(d.getDate() + 1);
+              console.log(nextDay);
+              $(".temp1").text("Day 1: "+response.list[0].weather[0].main);
+              $(".temp2").text("Day 2: "+response.list[1].weather[0].main);
+              $(".temp3").text("Day 3: "+response.list[2].weather[0].main);
+              $(".temp4").text("Day 4: "+response.list[3].weather[0].main);
+              $(".temp5").text("Day 5: "+response.list[4].weather[0].main);
+              
+    })
+};
